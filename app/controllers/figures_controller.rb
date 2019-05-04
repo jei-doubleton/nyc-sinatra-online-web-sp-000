2732b4fdb28baf161398de32f7d883c295917ba8
J. Singleton <jennifer.singleton@gmail.com>
2019-05-04 19:58:03 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,11 +7,18 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(params[figure])
+    @figure = Figure.create(params[:figure])
+
     if !params[:title][:name].empty?
       @figure.title = Title.create(params[:title])
       @figure.save
     end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmark = Landmark.create(params[:landmark])
+      @figure.save
+    end
+    
   end
 
 end

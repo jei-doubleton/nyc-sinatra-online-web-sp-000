b6dca9dde58e0e2cb93e6867854addc19ba022a0
J. Singleton <jennifer.singleton@gmail.com>
2019-05-04 18:52:42 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,7 +1,10 @@
 class CreateLandmarks < ActiveRecord::Migration
-  create_table :landmarks do |t|
-    t.string :name
-    t.integer :year_completed
-    t.integer :figure_id
+
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      t.integer :figure_id
+    end
   end
 end

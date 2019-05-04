dcbef63aaa45848fecc744e958fe6e23f367e8d3
J. Singleton <jennifer.singleton@gmail.com>
2019-05-04 21:19:52 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -14,4 +14,9 @@ class LandmarksController < ApplicationController
     erb :"landmarks/index"
   end
 
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    erb :"landmarks/show"
+  end
+
 end
